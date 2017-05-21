63641d33bdf91cef3cbdcc511ffc2227382b87d5
Jeremy Chone <jeremy.chone@gmail.com>
2017-05-21 15:01:32 -0700

    . add more unit test

--- test/test-listfiles.js
+++ test/test-listfiles.js
@@ -30,10 +30,10 @@ describe("listFiles", function(){
 	it("listFiles from", async () => {
 		var dirs = "./test/mock/";
 		var files = await fs.listFiles(dirs, {from: 0});
-		assert.equal(files.length, 4, "number of files from 0");
+		assert.equal(files.length, 5, "number of files from 0");
 
 		files = await fs.listFiles(dirs, {from: 2});
-		assert.equal(files.length, 2, "number of files from 2");		
+		assert.equal(files.length, 3, "number of files from 2");		
 	});	
 
 	it("listFiles to", async () => {
@@ -42,7 +42,7 @@ describe("listFiles", function(){
 		assert.equal(files.length, 3, "number of files to 2");
 
 		files = await fs.listFiles(dirs, {to: 10});
-		assert.equal(files.length, 4, "number of files to 10");		
+		assert.equal(files.length, 5, "number of files to 10");		
 	});
 
 	it("listFiles from to", async () => {
@@ -51,7 +51,7 @@ describe("listFiles", function(){
 		assert.equal(files.length, 2, "number of files from 1 to 2");
 
 		files = await fs.listFiles(dirs, {from:1, to: 10});
-		assert.equal(files.length, 3, "number of files from 1 to 10");		
+		assert.equal(files.length, 4, "number of files from 1 to 10");		
 	});
 
 	it("listFiles from to suffix", async () => {
