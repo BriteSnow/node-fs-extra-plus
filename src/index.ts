e83757ac0afd2723c61af0065eab169733cee8c0
Jeremy Chone <jeremy.chone@gmail.com>
2019-12-17 09:08:59 -0800

    + added fast-glob options type to .glob(pattern,cwdOrOptions)

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,12 @@ import { globCompare } from './util';
 /** 
  * Simplified and sorted glob function (using fast-glob) for one or more pattern from current directory or a optional cwd one. 
  * 
- * NOTE: The result will be sorted by natural directory/subdir/filename order (as a would a recursive walk)
+ * Note 1: The result will be sorted by natural directory/subdir/filename order (as a would a recursive walk)
+ * Note 2: When `cwd` in options, it is added to the file path i.e. `pathJoin(cwd, path)`
  * 
  * @returns always sorted result return Promise<string[]>
 */
-export async function glob(pattern: string | string[], cwdOrFastGlobOptions?: string): Promise<string[]> {
+export async function glob(pattern: string | string[], cwdOrFastGlobOptions?: string | Options): Promise<string[]> {
 	let opts: Options | undefined = undefined;
 
 	if (cwdOrFastGlobOptions != null) {
